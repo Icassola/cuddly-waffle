da6c264949c8ab24b22c405c4596ab385d0141ba
Isabella <i.l.cassolaberger@students.uu.nl>
2019-01-17 14:45:01 +0100

    .csv created. But now diacritics have to be removed

--- individual_assignment.py
+++ individual_assignment.py
@@ -1,13 +1,28 @@
 #Import the data from json, trim to only csv and save for random
 
 import json
-with open ('conflict_data_full_lined.json') as file:
+
+with open ('conflict_data_full_lined.json', encoding=utf-8) as file:
     raw_data_dict = json.load (file) 
 
-conflict_venezuela = []
+venezuela_data = []
 
 for observation in raw_data_dict:
     if observation['country'] == 'Venezuela':
-        conflict_venezuela.append(observation)
-print(conflict_venezuela)
+        venezuela_data.append(observation)
+print(venezuela_data)
 
+import csv
+header = []
+
+for observation in venezuela_data:
+    for key in observation.keys(): 
+        if key not in header:
+            header.append(key)
+with open('venezuela_data.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=header, lineterminator='\n', 
+        delimiter=',')
+    writer.writeheader()
+
+    for observation in venezuela_data:
+        writer.writerow(observation)
