13e447949552890cf5c155e449f44573094f44c2
Isabella <i.l.cassolaberger@students.uu.nl>
2019-01-18 10:49:08 +0100

    Writing the report

--- individual_assignment.r
+++ individual_assignment.r
@@ -3,8 +3,17 @@
 library(readr)
 library(ggplot2)
 
-venezdata <- venezuela_data
+venezdata <- read.csv('venezuela_data', stringsAsFactors=FALSE) 
+
+events_per_year <- count(venezdata, year)
+deaths_per_year <- count()
 
 ggplot() + 
-  geom_bar(data = venezdata, mapping = aes(x = year, colour = deaths_civilians))
+  geom_bar(data = venezdata, mapping = aes(x = year))
 
+
+ggplot(data = venezdata) +
+  geom_point(mapping = aes(x=year, y= deaths_civilians))
+
+ggplot(data = venezdata) +
+  geom_bar(mapping =aes(x=year, y=events_per_year) (stat= 'identity'))
